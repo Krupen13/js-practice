aa0c86c091ad0d06ccf3ac03fc715e1ca64905be
Krupen13 <66956397+Krupen13@users.noreply.github.com>
2024-06-11 12:11:53 +0000

    new variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "kruzpatel39@gmail.com"
+var accountPassword = "12345"
+let accountCity = "Surat"
+
+let accountState;
+
+accountEmail = "k@p.com"
+accountCity = "Mumbai"
+// accountId = 2
+
+console.log(accountState);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
