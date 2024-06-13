0bc87214792b81e6d01eb7b5a6d68b6363517854
Krupen13 <66956397+Krupen13@users.noreply.github.com>
2024-06-13 21:30:24 +0000

    timezone

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,29 @@
+let myDate = new Date()
+
+// console.log(myDate.toString())
+
+// console.log(myDate.toDateString())
+
+// console.log(myDate.toLocaleString())
+
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2024, 1, 13, 5, 3)
+
+// console.log(myCreatedDate.toLocaleString());
+
+// let timeStamp = Date.now()
+
+// console.log(Math.floor(Date.now()/1000));
+
+ let newDate = new Date()
+
+// console.log(newDate);
+// console.log(newDate.getFullYear() + 1);
+
+ console.log(newDate.toLocaleString('default', {
+     weekday: "long",
+     
+    
+ }))
+
