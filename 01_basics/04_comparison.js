4ea47ba4186f34fcb2b1d80b94e3665e2fc549e2
Krupen13 <66956397+Krupen13@users.noreply.github.com>
2024-06-13 13:37:32 +0000

    New Commit

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,4 +1,12 @@
 const id = Symbol('123')
 const id2 = Symbol('123')
 
-console.log(id === id2);
+console.log(id != id2);
+
+const gameName = new String('krupen')
+
+console.log(gameName[2]);
+
+const newString = gameName.slice(0, 3)
+
+console.log(newString);
