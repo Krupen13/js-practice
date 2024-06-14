55ee5e8919e1b6d86176e18cde4615a2194b2d69
Krupen13 <66956397+Krupen13@users.noreply.github.com>
2024-06-14 18:01:40 +0000

    arrays prc

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+
+
+const myArrays = [0,1,2,3,4,5]
+
+const heroes = ["Krupen", "shArk"]
+
+const myArr = new Array(6,7,8)
+
+//console.log(myArrays[4]);
+
+
+// myArrays.shift()
+
+// console.log(myArrays.indexOf(4));
+
+// const newArr = myArrays.join()
+
+// console.log(myArrays);
+
+// console.log(newArr);
+
+console.log("a", myArrays)
+
+const myn1 = myArrays.slice(1,5)
+
+console.log(myn1);
+
+console.log("b", myArrays);
+
+const myn2 = myArrays.splice(1,3)
+
+console.log(myn2);
+
+console.log("splice", myArrays);
