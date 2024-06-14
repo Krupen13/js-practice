791982020b9f5521c4fbb2864b52f7fbb97b1991
Krupen13 <66956397+Krupen13@users.noreply.github.com>
2024-06-14 18:34:25 +0000

    Arrays.js

---
+++ 02_basics/02_array.js
@@ -0,0 +1,35 @@
+const marvel = ["thor", "ironman", "hulk"]
+
+const dc = ["supaman", "flush", "betsman"]
+
+//marvel.push(dc)
+
+// console.log(marvel);
+
+
+// const all = marvel.concat(dc)
+
+// console.log(all);
+// const all = [...marvel, ...dc]
+
+// console.log(all);
+
+// const anoArr = [1,2,3, [4,5,6], 7, [6,7, [4,5]]]
+
+// const realAnoArr = anoArr.flat(Infinity)
+
+// console.log(realAnoArr);
+
+
+
+console.log(Array.isArray([1,2,3]))
+
+console.log(Array.from("KP"))
+
+console.log(Array.from({name: "Krupen"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
