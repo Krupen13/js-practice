0663bb9ccbf6c012f4d1e8e6720bdd093f7a5549
Krupen13 <66956397+Krupen13@users.noreply.github.com>
2024-06-16 02:11:15 +0000

    function depth

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,7 +20,7 @@ function addTwoNums(number1, number2) {
 
 const result = addTwoNums(10,1)
 
-console.log(result)
+// console.log(result)
 
 
 function loginUser(username = "Krupen") {
@@ -33,4 +33,35 @@ return `${username} has just Logged In`
 }
 }
 
-console.log(loginUser())
+// console.log(loginUser())
+
+function calculateCart(v1,...num1) {
+return num1
+}
+
+// console.log(calculateCart(200,300,400))
+
+const user = {
+    username: "Krupen",
+    age: 25
+}
+
+function handleObject(anyOb) {
+    console.log(`Username is ${anyOb.username} and age is ${anyOb.age}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "Kru",
+    age: 26
+})
+
+const myNewArray = [2,4,6,8]
+
+function retSecVal(getArray) {
+    return getArray[2]
+}
+// console.log(retSecVal(myNewArray));
+
+console.log(retSecVal([1,3,5,7]))
